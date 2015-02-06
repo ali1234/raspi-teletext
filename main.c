d6fc247958a5fbcf1cd353d5f715745f7d1401da
Alistair Buxton <a.j.buxton@gmail.com>
2015-02-06 00:35:53 +0000

    It works!

--- main.c
+++ main.c
@@ -7,6 +7,7 @@
 
 #include "bcm_host.h"
 
+#include "hamming.h"
 
 #ifndef ALIGN_UP
 #define ALIGN_UP(x,y)  ((x + (y)-1) & ~((y)-1))
@@ -42,6 +43,25 @@ int main(void)
                              0 };
 
     unsigned char packet[45] = {0x55, 0x55, 0x27};
+
+    // mrag
+    packet[3] = hamming84(1);
+    packet[4] = hamming84(0);
+
+    // page-subpage
+    packet[5] = hamming84(0);
+    packet[6] = hamming84(0);
+    packet[7] = hamming84(0);
+    packet[8] = hamming84(0);
+    packet[9] = hamming84(0);
+    packet[10] = hamming84(0);
+    packet[11] = hamming84(0);
+    packet[12] = hamming84(0);
+
+    char message[32] = "Hello Raspberry Pi ABCDEFGHIJ";
+    int z;
+    for (z=0; z<32;z++)
+        packet[13+z] = parity(message[z]);
 
     vars = &gRectVars;
 
@@ -92,13 +112,14 @@ int main(void)
                                             vars->image,
                                             &dst_rect );
     assert( ret == 0 );
+
     vars->update = vc_dispmanx_update_start( 10 );
     assert( vars->update );
 
 
 
     vc_dispmanx_rect_set( &src_rect, 0, 0, width << 16, height << 16 );
-    vc_dispmanx_rect_set( &dst_rect, 18, 0, 620, 64 );
+    vc_dispmanx_rect_set( &dst_rect, 10, 0, 700, 64 );
     vars->element = vc_dispmanx_element_add(    vars->update,
                                                 vars->display,
                                                 2000,               // layer
@@ -113,10 +134,27 @@ int main(void)
     ret = vc_dispmanx_update_submit_sync( vars->update );
     assert( ret == 0 );
 
+
     while(1) {
+
+/*        for(x = 0;x < 45; x+=1) {
+            for (xx = 0; xx < 8; xx++) {
+                n = 2 * ((x * 8) + xx);
+                if ((packet[x] >> xx)&1) {
+                    ((unsigned char *)(vars->image))[n] = 0xff;
+                    ((unsigned char *)(vars->image))[n+1] = 0xff;
+                } else {
+                    ((unsigned char *)(vars->image))[n] = 0x0;
+                    ((unsigned char *)(vars->image))[n+1] = 0x0;
+                }
+            }
+        }
+*/
+
         sleep(1);
 
     }
+
 
     vars->update = vc_dispmanx_update_start( 10 );
     assert( vars->update );
