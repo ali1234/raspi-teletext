bf1bf93f10ca229931967a8dcf27eaac31adf304
Alistair Buxton <a.j.buxton@gmail.com>
2015-02-07 00:15:33 +0000

    Unexpected results.

--- main.c
+++ main.c
@@ -12,24 +12,8 @@
 #define ALIGN_UP(x,y)  ((x + (y)-1) & ~((y)-1))
 #endif
 
-/*
-typedef struct
-{
-    DISPMANX_DISPLAY_HANDLE_T   display;
-    DISPMANX_MODEINFO_T         info;
-    void                       *image;
-    DISPMANX_UPDATE_HANDLE_T    update;
-    DISPMANX_RESOURCE_HANDLE_T  resource;
-    DISPMANX_ELEMENT_HANDLE_T   element;
-    uint32_t                    vc_image_ptr;
-
-} RECT_VARS_T;
-
-static RECT_VARS_T  gRectVars;
-*/
 
 static DISPMANX_RESOURCE_HANDLE_T  resource[3];
-static int next_resource = 0;
 static void *image;
 
 static DISPMANX_DISPLAY_HANDLE_T   display;
@@ -41,35 +25,10 @@ static unsigned short pal[256] = {
     RGB565(0,0,0),
     RGB565(255,255,255),
     RGB565(255,0,0),
+    RGB565(0,255,0),
+    RGB565(0,0,255),
 };
 
-void vsync(DISPMANX_UPDATE_HANDLE_T u, void* arg) {
-
-    int ret;
-    DISPMANX_UPDATE_HANDLE_T    update;
-
-    update = vc_dispmanx_update_start( 10 );
-    assert( update );
-
-    ret = vc_dispmanx_element_change_source( update, element, resource[next_resource]);
-    assert( ret == 0 );
-
-    ret = vc_dispmanx_update_submit_sync( update );
-    assert( ret == 0 );
-
-    if(next_resource != 2) {
-
-        int real_next_resource = next_resource ^ 1;
-        next_resource = 2; // use filler if next callback called before this one ends
-
-        // fill image
-        // write to resource
-
-        next_resource = real_next_resource; // queue up next real resource
-
-    }
-
-}
 
 int main(void)
 {
@@ -81,10 +40,6 @@ int main(void)
     DISPMANX_UPDATE_HANDLE_T    update;
     uint32_t                    vc_image_ptr;
 
-    VC_DISPMANX_ALPHA_T alpha = { DISPMANX_FLAGS_ALPHA_FROM_SOURCE | DISPMANX_FLAGS_ALPHA_FIXED_ALL_PIXELS,
-                             255, /*alpha 0->255*/
-                             0 };
-
     bcm_host_init();
 
     display = vc_dispmanx_display_open( 0 );
@@ -92,13 +47,15 @@ int main(void)
     int width = 360;
     int height = 16;
     int pitch = ALIGN_UP(width, 32);
-    //int aligned_height = ALIGN_UP(height, 16);
 
-    image = calloc( 1, pitch * height ); // buffer 0
+    image = calloc( 1, pitch * height );
     assert(image);
 
     vc_dispmanx_rect_set( &dst_rect, 0, 0, width, height);
 
+
+
+    memset(image, 2, pitch * height); //red
     resource[0] = vc_dispmanx_resource_create( type, width, height, &vc_image_ptr );
     assert( resource[0] );
     ret = vc_dispmanx_resource_set_palette(  resource[0], pal, 0, sizeof pal );
@@ -106,8 +63,10 @@ int main(void)
     ret = vc_dispmanx_resource_write_data(  resource[0], type, pitch, image, &dst_rect );
     assert( ret == 0 );
 
-    memset(image, 1, pitch * height); // buffer 1
+/*
+    // uncomment this code for unexpected results
 
+    memset(image, 3, pitch * height); // green
     resource[1] = vc_dispmanx_resource_create( type, width, height, &vc_image_ptr );
     assert( resource[1] );
     ret = vc_dispmanx_resource_set_palette(  resource[1], pal, 0, sizeof pal );
@@ -115,41 +74,36 @@ int main(void)
     ret = vc_dispmanx_resource_write_data(  resource[1], type, pitch, image, &dst_rect );
     assert( ret == 0 );
 
-    memset(image, 2, pitch * height); // filler
 
+    memset(image, 4, pitch * height); // blue
     resource[2] = vc_dispmanx_resource_create( type, width, height, &vc_image_ptr );
     assert( resource[2] );
     ret = vc_dispmanx_resource_set_palette(  resource[2], pal, 0, sizeof pal );
     assert( ret == 0 );
     ret = vc_dispmanx_resource_write_data(  resource[2], type, pitch, image, &dst_rect );
     assert( ret == 0 );
-
-    memset(image, 1, pitch * height);
+*/
 
 
     update = vc_dispmanx_update_start( 10 );
     assert( update );
 
-
-
     vc_dispmanx_rect_set( &src_rect, 0, 0, width << 16, height << 16 );
-    vc_dispmanx_rect_set( &dst_rect, 10, 0, 700, 64 );
+    vc_dispmanx_rect_set( &dst_rect, 100, 100, 500, 400 );
     element = vc_dispmanx_element_add( update, display,
                                        2000,               // layer
-                                       &dst_rect, resource[2], &src_rect,
+                                       &dst_rect, resource[0], &src_rect,
                                        DISPMANX_PROTECTION_NONE,
-                                       &alpha, NULL, VC_IMAGE_ROT0 );
+                                       NULL, NULL, VC_IMAGE_ROT0 );
 
     ret = vc_dispmanx_update_submit_sync( update );
     assert( ret == 0 );
 
-    vc_dispmanx_vsync_callback(display, vsync, NULL);
 
     while(1) {
         sleep(1);
     }
 
-    vc_dispmanx_vsync_callback(display, NULL, NULL); // disable callback
 
     update = vc_dispmanx_update_start( 10 );
     assert( update );
