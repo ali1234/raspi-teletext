552d66195cdb7e294211eae4096972f5bdb71a5c
Alistair Buxton <a.j.buxton@gmail.com>
2016-01-31 00:33:58 +0000

    Add support for NTSC captions to tvctl.
    This shifts the picture by 2 lines per field instead of 16 because
    NTSC captions only use 1 line per field, but we must skip 1 line
    in the top field.

--- tvctl.c
+++ tvctl.c
@@ -42,6 +42,8 @@
 #define MAP_SIZE 4096UL
 #define MAP_MASK (MAP_SIZE - 1)
 
+typedef enum {PAL_OFF, PAL_ON, NTSC_OFF, NTSC_ON, UNKNOWN} tstate;
+
 int try_set_regs(volatile unsigned int *regs, int argc, char *argv[])
 {
     // each register is two shorts
@@ -55,23 +57,35 @@ int try_set_regs(volatile unsigned int *regs, int argc, char *argv[])
 
     // question: what is the difference between the two values in reg[5]/reg[7] register?
 
-    int state;
+    tstate state;
 
     if (regs[5] == 0x00140003 && regs[6] == 0x00020120 && regs[7] == 0x00130003 && regs[8] == 0x00020120)
-        state = 0;
+        state = PAL_OFF;
     else if (regs[5] == 0x00040003 && regs[6] == 0x00120120 && regs[7] == 0x00030003 && regs[8] == 0x00120120)
-        state = 1;
+        state = PAL_ON;
+    else if (regs[5] == 0x00100003 && regs[6] == 0x000300f0 && regs[7] == 0x00100003 && regs[8] == 0x000400f0)
+        state = NTSC_OFF;
+    else if (regs[5] == 0x000e0003 && regs[6] == 0x000500f0 && regs[7] == 0x000e0003 && regs[8] == 0x000600f0)
+        state = NTSC_ON;
     else
-        state = 2;
+        state = UNKNOWN;
 
     if (argc == 2 && argv[1][1] == 'n') {
         switch(state) {
-            case 0:
+            case PAL_OFF:
                 regs[5] = 0x00040003;
                 regs[6] = 0x00120120;
                 regs[7] = 0x00030003;
                 regs[8] = 0x00120120;
-            case 1:
+                fprintf(stderr, "Teletext output is now on.\n");
+                return 1;
+            case NTSC_OFF:
+                regs[5] = 0x000e0003;
+                regs[6] = 0x000500f0;
+                regs[7] = 0x000e0003;
+                regs[8] = 0x000600f0;
+            case PAL_ON:
+            case NTSC_ON:
                 fprintf(stderr, "Teletext output is now on.\n");
                 return 1;
             default:
@@ -81,12 +95,20 @@ int try_set_regs(volatile unsigned int *regs, int argc, char *argv[])
 
     if (argc == 2 && argv[1][1] == 'f') {
         switch(state) {
-            case 1:
+            case PAL_ON:
                 regs[5] = 0x00140003;
                 regs[6] = 0x00020120;
                 regs[7] = 0x00130003;
                 regs[8] = 0x00020120;
-            case 0:
+                fprintf(stderr, "Teletext output is now off.\n");
+                return 1;
+            case NTSC_ON:
+                regs[5] = 0x00100003;
+                regs[6] = 0x000300f0;
+                regs[7] = 0x00100003;
+                regs[8] = 0x000400f0;
+            case PAL_OFF:
+            case NTSC_OFF:
                 fprintf(stderr, "Teletext output is now off.\n");
                 return 1;
             default:
@@ -123,7 +145,7 @@ int main(int argc, char **argv)
         }
 
         if(!try_set_regs(map_base, argc, argv)) {
-            fprintf(stderr, "Could not find registers. Make sure TV out is set to PAL mode.\n");
+            fprintf(stderr, "Could not find registers. Make sure composite video out is enabled.\n");
         }
 
     }
